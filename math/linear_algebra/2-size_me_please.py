ba428ce16dcc60b409a4fcd327022f3206bc001d
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2025-05-24 10:42:24 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
 def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0] if matrix else []
     return shape
+
+matrix_shape = __import__('2-size_me_please').matrix_shape
 
 mat1 = [[1, 2], [3, 4]]
 print(matrix_shape(mat1))
